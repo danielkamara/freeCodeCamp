60dc6afb2cdc47c13d96a18bc9da2b426507bd50
Daniel <danlikestech@gmail.com>
2019-05-20 18:30:20 -0400

    added while loop-solution

--- algorithmsAndDataStructures/basicAlgorithmScripting/factorialize.js
+++ algorithmsAndDataStructures/basicAlgorithmScripting/factorialize.js
@@ -1,10 +1,26 @@
 function factorialize(num) {
-  var answer = 0;
+  var answer = 1;
 for (var i = 1; i <= num; i++) {
 answer *= i;
 }
 
   return answer;
+}
+
+factorialize(5);
+
+
+// 2nd Solution using while loop;
+
+
+function factorialize(num) {
+  var answer = 1;
+  while (num > 1){
+  answer *= num
+  num--
+  }
+  return answer;
+
 }
 
 factorialize(5);
