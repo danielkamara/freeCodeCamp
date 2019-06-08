b2b782ad606ea65c6dd368295498b82bc75f6a5d
kamishere <danlikestech@gmail.com>
2019-06-08 19:47:22 -0400

    add new files

--- algorithmsAndDataStructures/basicAlgorithmScripting/repeatAstring.js
+++ algorithmsAndDataStructures/basicAlgorithmScripting/repeatAstring.js
@@ -1,6 +1,6 @@
 // Basic Algorithm Scripting: Repeat a String Repeat a String
-// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
-
+// Repeat a given string str (first argument) for num times (second argument). 
+// Return an empty string if num is not a positive number.
 // Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
 function repeatStringNumTimes(str, num) {
