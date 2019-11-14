476c9979d20bed9f8774197c11484bea97916a71
Daniel <danlikestech@gmail.com>
2019-11-13 21:07:01 -0500

    added more challenges & solved 1 challenge

--- IntermediateProgramming/sumNumberInARange.js
+++ IntermediateProgramming/sumNumberInARange.js
@@ -6,18 +6,8 @@
 
 
 
-
 function sumAll(arr) {
-  var start = Math.min(arr[0], arr[1]);
-  var end = Math.max(arr[0], arr[1]);
-  var total = 0;
-
-  for (var i = start; i <= end; i++) {
-    total += i;
-  }
-    
-return total;
-
+    return 1;
   }
   
-  sumAll([4, 1]);
+  sumAll([1, 4]);
