19ced34788301f79cb1e372f5052d758451bdbdb
Daniel <danlikestech@gmail.com>
2019-11-15 08:31:49 -0500

    solved challenge

--- IntermediateProgramming/02_diff_two_arrays.js
+++ IntermediateProgramming/02_diff_two_arrays.js
@@ -4,12 +4,14 @@
 
 // Note
 // You can return the array with its elements in any order.
-
 function diffArray(arr1, arr2) {
-    var newArr = [];
-    // Same, same; but different.
-    return newArr;
-  }
-  
-  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+  var newArr = arr1.concat(arr2);
+  // Same, same; but different.
+  return newArr.filter(function(value, index, array) {
+    if (array.slice(index + 1).indexOf(value) === -1 && array.slice(0, index).indexOf(value) === -1) {
+      return value;
+    }
+  });
+}
 
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
