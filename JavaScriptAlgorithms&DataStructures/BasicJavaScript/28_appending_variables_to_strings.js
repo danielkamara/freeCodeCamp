4cf7fd4e0cdca8b2df738f69f393146c08f5df5c
Daniel <danlikestech@gmail.com>
2020-04-18 12:09:34 -0500

    appended an adjective to a string using += operator

---
+++ JavaScriptAlgorithms&DataStructures/BasicJavaScript/28_appending_variables_to_strings.js
@@ -0,0 +1,19 @@
+/*
+Basic JavaScript: Appending Variables to StringsPassed
+Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals( += ) operator.
+
+Example:
+
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+// ourStr is now "freeCodeCamp is awesome!"
+Set someAdjective and append it to myStr using the += operator.
+*/
+
+// Change code below this line
+
+var someAdjective = "Awesome!!!";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
