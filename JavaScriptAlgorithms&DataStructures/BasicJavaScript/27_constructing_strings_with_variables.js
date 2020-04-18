912289b3cfe5850e20799cba321bca149c25a84d
Daniel <danlikestech@gmail.com>
2020-04-18 12:04:23 -0500

    constructed 2 string variables

---
+++ JavaScriptAlgorithms&DataStructures/BasicJavaScript/27_constructing_strings_with_variables.js
@@ -0,0 +1,21 @@
+/* 
+Basic JavaScript: Constructing Strings with VariablesPassed
+Sometimes you will need to build a string, Mad Libs style.By using the concatenation operator(+), you can insert one or more variables into a string you 're building.
+
+Example:
+
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+ourStr is now "Hello, our name is freeCodeCamp, how are you?"
+Set myName to a string equal to your name and build myStr with myName between the strings "My name is "
+and " and I am well!"
+*/
+
+// Only change code below this line
+var myName = "Daniel Kamara";
+var myStr = "My name is " + myName + " and I am well!";
+
+
+
+
+
