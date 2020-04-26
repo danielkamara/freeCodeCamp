2fd8b87c0ea106c3b926ab6342cbbc01c396cfe5
Daniel <danlikestech@gmail.com>
2020-04-25 19:54:26 -0500

    nested 2 arrays in a single array

---
+++ JavaScriptAlgorithms&DataStructures/BasicJavaScript/37_nest_one_array_within_array.js
@@ -0,0 +1,15 @@
+/*  
+Basic JavaScript: Nest one Array within Another ArrayPassed
+You can also nest arrays within other arrays, like below:
+
+    [
+        ["Bulls", 23],
+        ["White Sox", 45]
+    ]
+This is also called a multi - dimensional array.
+
+Create a nested array called myArray.
+*/
+
+// Only change code below this line
+var myArray = [['Daniel'], [25]];
