52f840226c9883663218b24893d9ba0853eb55d5
Daniel <danlikestech@gmail.com>
2020-04-25 21:53:49 -0500

    added subArrays to shopping list

---
+++ JavaScriptAlgorithms&DataStructures/BasicJavaScript/45_shopping_list.js
@@ -0,0 +1,19 @@
+/* 
+Basic JavaScript: Shopping ListPassed
+Create a shopping list in the variable myList.The list should be a multi - dimensional array containing several sub - arrays.
+
+The first element in each sub - array should contain a string with the name of the item.The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+
+There should be at least 5 sub - arrays in the list.
+
+*/
+
+var myList = [
+    ["Chocolate Bar", 15],
+    ["Canned Milk", 3],
+    ["Bread", 2],
+    ["Eggs", 3],
+    ["Water", 5]
+];
